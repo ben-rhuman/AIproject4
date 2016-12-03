6bf30377aa7207bcdd2ef345351e7ae031cc65d8
k28h885 <k28h885@bz-cs-cslab12.msu.montana.edu>
2016-12-03 10:57:39 -0700

    Added policy classes

---
+++ src/policyPackage/IPolicyAlgorithm.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package policyPackage;
+
+/**
+ *
+ * @author k28h885
+ */
+public interface IPolicyAlgorithm {
+    public void ASK();
+    public void TELL();
+}
---
+++ src/policyPackage/ValueIteration.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package policyPackage;
+
+/**
+ *
+ * @author k28h885
+ */
+public class ValueIteration implements IPolicyAlgorithm{
+
+    @Override
+    public void ASK() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
